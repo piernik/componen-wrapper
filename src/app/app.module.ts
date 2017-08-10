216bd166fa29f29131a6bd98290fffbb84b5d588
piernik <lpiernik@interia.pl>
2017-08-10 16:09:40 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
+import {ComponentWrapperModule} from '@lukana/component-wrapper';
+// import {ComponentWrapperModule} from "../../my-lib/src/component-wrapper.module";
+
 import {AppComponent} from './app.component';
-// import {ComponentWrapperModule} from '@lukana/component-wrapper';
 import {TestModule} from "./test/test.module";
 import {TestComponent} from "./test/test.component";
-import {ComponentWrapperModule} from "../../my-lib/src/component-wrapper.module";
 
 @NgModule({
     declarations: [
